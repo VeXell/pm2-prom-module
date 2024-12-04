8fcaa081d7d3cb4721ee9a2f2c5cb1db33824017
Viacheslav Volkov <vexell@gmail.com>
2024-12-04 22:40:06 +0000

    Get free memory

--- utils/docker.ts
+++ utils/docker.ts
@@ -1,4 +1,5 @@
-import { access, constants } from 'node:fs/promises';
+import { access, constants, readFile } from 'node:fs/promises';
+import os from 'node:os';
 
 const MEMORY_AVAILABLE = '/sys/fs/cgroup/memory/memory.limit_in_bytes';
 const MEMORY_USED = '/sys/fs/cgroup/memory/memory.usage_in_bytes';
@@ -9,5 +10,46 @@ export const hasDockerLimitFiles = async () => {
         return true;
     } catch {
         return false;
+    }
+};
+
+export const getTotalMemory = async () => {
+    try {
+        const data = await readFile(MEMORY_AVAILABLE, { encoding: 'utf8' });
+
+        if (data === 'max') {
+            return os.totalmem();
+        } else {
+            const memoryNumber = parseInt(data, 10);
+
+            if (isNaN(memoryNumber)) {
+                return 0;
+            } else {
+                return memoryNumber;
+            }
+        }
+    } catch {
+        return 0;
+    }
+};
+
+export const getFreeMemory = async () => {
+    try {
+        const totalMemory = await getTotalMemory();
+
+        if (totalMemory > 0) {
+            const data = await readFile(MEMORY_USED, { encoding: 'utf8' });
+            const usedMemory = parseInt(data, 10);
+
+            if (isNaN(usedMemory)) {
+                return 0;
+            } else {
+                return totalMemory - usedMemory;
+            }
+        } else {
+            return 0;
+        }
+    } catch {
+        return 0;
     }
 };
